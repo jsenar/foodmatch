1988ef41d1350ab0bdd700e4df7fc6ebaf1f77d2
John <JLSenar@gail.com>
2019-02-22 16:19:08 -0800

    Adds yelp search functionality to GroupCreate component

--- client/components/GroupCreate.jsx
+++ client/components/GroupCreate.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import SearchForm from './SearchForm';
+import axios from 'axios';
 
-function search({term, location}) {
-  console.log(term);
-  console.log(location);
+class GroupCreate extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      queriedBusinesses: [],
+      savedBusinesses: []
+    };
+
+    this.search = this.search.bind(this);
+  }
+
+  search({term, location}) {
+    const locale = 'en_US';
+    axios.post('/api/search',{
+      term,
+      location,
+      locale
+    }).then((res) => {
+      this.setState({queriedBusinesses: res.data.search.business});
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Search for Restaurants</h3>
+        <SearchForm search={this.search}/>
+        {/* {this.state.queriedBusinesses.map( (business) => {
+          return (
+            <h4 key={business.alias}>{business.name}</h4>
+          )
+        })} */}
+        {/* Search Results */}
+      </div>
+    );
+  }
 }
-
-function GroupCreate() {
-  return (
-    <div>
-      <h3>Search for Restaurants</h3>
-      <SearchForm search={search}/>
-      {/* Search List */}
-      {/* Search Results */}
-    </div>
-  );
-}
-
-
 
 export default GroupCreate;
